5fe006af880c25329b309ef236f5042927b0d6dc
jvtsp <jvtsp1999@gmail.com>
2021-03-27 22:22:02 -0300

    Script remontado

--- llvm.sh
+++ llvm.sh
@@ -3,7 +3,7 @@
 #       Created by João Vitor Pereira
 #       Date 25/03/2021
 # Aumentar Partições Linux usando LVM
-#######################################3
+#######################################
 clear
 echo "Partições montadas: "
 df -h
@@ -11,5 +11,22 @@ echo
 echo
 echo "Path LVMs disponíveis"
 lvdisplay |grep "LV Path"
-read -p "Insira o nome do path que precisa aumentar: " PATH
-echo $PATH
+
+function FS(){
+        /bin/clear
+        echo "dados do FS"
+}
+
+i=1
+while [ $i -le 5 ]; do
+        read -p "Insira a montagem que deseja aumentar: " PATH
+        if [ -z $PATH ]; then
+                echo "Caminho vazio"
+                i=$(($i+1))
+        else
+                echo "$PATH gravado"
+                i=6
+                FS
+        fi
+done
+
