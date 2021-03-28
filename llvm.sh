2b50dc505dff8b9920fbfff50e894602c5cdeca6
jvtsp <jvtsp1999@gmail.com>
2021-03-28 11:17:27 -0300

    funcionalidades
    Adicionando funções para descobrir o Vg que o usuário precisa expandir

--- llvm.sh
+++ llvm.sh
@@ -4,29 +4,30 @@
 #       Date 25/03/2021
 # Aumentar Partições Linux usando LVM
 #######################################
+
 clear
 echo "Partições montadas: "
-df -h
+df -Th
 echo
 echo
 echo "Path LVMs disponíveis"
 lvdisplay |grep "LV Path"
 
 function FS(){
-        /bin/clear
-        echo "dados do FS"
+        PATH1=$(PATH)
 }
 
 i=1
 while [ $i -le 5 ]; do
-        read -p "Insira a montagem que deseja aumentar: " PATH
+        read -p "Insira o caminho completo da partição LVM: " PATH
         if [ -z $PATH ]; then
                 echo "Caminho vazio"
                 i=$(($i+1))
         else
                 echo "$PATH gravado"
                 i=6
-                FS
+                /bin/clear
+                VG1=$(/sbin/lvdisplay $PATH |/bin/grep "VG Name" |/bin/awk '{$2=$2};1' |/bin/cut -d " " -f 3)
+                /sbin/vgdisplay $VG1|/bin/grep "Free  PE / Size" |/bin/awk '{$2=$2};1' |/bin/cut -d " " -f 7
         fi
 done
-
