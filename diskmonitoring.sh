9f5673bc917efed709ab11028dd94e88068f2726
jvtsp <jvtsp1999@gmail.com>
2021-05-26 20:29:36 -0300

    Correção do FIND

--- diskmonitoring.sh
+++ diskmonitoring.sh
@@ -7,27 +7,22 @@
 #       Ao atingir o limite de 98% de uso do /BACKUP -> Purge nos arquivos dentro do
 ####################################################################
 #Informar dentro das '' o FS que precisa ser monitorado
-used=$(df -Ph | grep '/dev/loop0' | awk {'print $5'})
+used=$(df -Ph | grep '' | awk {'print $5'})
 #Limite ajustado com a váriavel abaixo
-max=80%
+max=96%
 #stable=70%
-#Diretório que contém os Dumps
-pumpdiario=/BACKUP/PumpDiario
-pumpsemana=/BACKUP/PumpSemanal
-pumpdiariopsbk=/BACKUP/psbk/PumpDiario
-pumpsemanalpsbk=/BACKUP/psbk/PumpSemanal
-# Extensão dos arquivos que serão apagados
-type1=dmp
-type2=dmp.gz
 # Ao atingir o limite informado na váriavel 'max' ele executa a geração de Logs e o purge dos dados contidos nos diretórios acima
 if [[ ${used%?} -ge ${max%?} ]]; then
-        echo "The Mount Point "BACKUP" on $(hostname) has used $used at $(date)" >> /var/log/diskclean.log
-        find $pumpdiario -type f -name "*.$type1"  -atime +1 -exec rm {} \;
-        find $pumpdiario -type f -name "*.$type2"  -atime +1 -exec rm {} \;
-        find $pumpsemana -type f -name "*.$type1"  -atime +1 -exec rm {} \;
-        find $pumpsemana -type f -name "*.$type2"  -atime +1 -exec rm {} \;
-        find $pumpdiariopsbk -type f -name "*.$type1"  -atime +1 -exec rm {} \;
-        find $pumpdiariopsbk -type f -name "*.$type2"  -atime +1 -exec rm {} \;
-        find $pumpsemanalpsbk -type f -name "*.$type1"  -atime +1 -exec rm {} \;
-        find $pumpsemanalpsbk -type f -name "*.$type2"  -atime +1 -exec rm {} \;
+        echo "The Mount Point "BACKUP" on $(hostname) has used $used at $(date)" >> /tmp/diskclean.log
+        find /BACKUP/PumpDiario -daystart -type f -name '*.dmp'  -mtime 1 -exec rm {} \;
+        find /BACKUP/PumpDiario -daystart -type f -name '*.dmp.gz'  -mtime 1 -exec rm {} \;
+
+        find /BACKUP/PumpSemanal -daystart -type f -name '*.dmp'  -mtime 1 -exec rm {} \;
+        find /BACKUP/PumpSemanal -daystart -type f -name '*.dmp.gz'  -mtime 1 -exec rm {} \;
+
+        find /BACKUP/psbk/PumpDiario -daystart -type f -name '*.dmp'  -mtime 1 -exec rm {} \;
+        find /BACKUP/psbk/PumpDiario -daystart -type f -name '*.dmp.gz'  -mtime 1 -exec rm {} \;
+
+        find /BACKUP/psbk/PumpSemanal -daystart -type f -name '*.dmp'  -mtime 1 -exec rm {} \;
+        find /BACKUP/psbk/PumpSemanal -daystart -type f -name '*.dmp.gz'  -mtime 1 -exec rm {} \;
 fi
